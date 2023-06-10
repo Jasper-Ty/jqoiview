acb32c7abfcbec8da2e5ce09bdde92ffdfbb0d96
Jasper <jasperty2@gmail.com>
2023-06-09 21:30:07 -0400

    Got rid of decoder code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,12 @@ use std::{
 };
 
 use quite_ok_image::{
+    Chunk::*,
     qoi::QoiHeader,
     decode::Decoder,
     BytesToChunks,
+    Pix,
+    hash,
 };
 
 use sdl2::{
@@ -40,8 +43,6 @@ fn main() -> Result<(), Box<dyn error::Error>>{
     let width = header.width;
     let height = header.height;
     
-    println!("{:?}", header);
-
     let metadata = f.metadata()?;
     let chunks_len = metadata.len() - 22;
     f.seek(SeekFrom::Start(14))?;
@@ -52,43 +53,75 @@ fn main() -> Result<(), Box<dyn error::Error>>{
         .map(|b| b.unwrap())
         .chunks();
 
-    let mut decode = Decoder::new(chunks);
+    let mut curr: Pix = (0, 0, 0, 255);
+    let mut index = [(0, 0, 0, 0); 64];
+    let mut pixels: Vec<u8> = Vec::with_capacity((4*width*height) as usize);
+
+    for chunk in chunks {
+        curr = match chunk {
+            RGB(r, g, b) => (r, g, b, curr.3),
+            RGBA(r, g, b, a) => (r, g, b, a), 
+            INDEX(i) => index[i as usize],
+            DIFF(dr, dg, db) => (
+                curr.0
+                    .wrapping_add(dr)
+                    .wrapping_sub(2),
+                curr.1
+                    .wrapping_add(dg)
+                    .wrapping_sub(2),
+                curr.2
+                    .wrapping_add(db)
+                    .wrapping_sub(2),
+                curr.3,
+            ),
+            LUMA(dg, drdg, dbdg) => (
+                curr.0
+                    .wrapping_add(dg)
+                    .wrapping_sub(40)
+                    .wrapping_add(drdg),
+                curr.1
+                    .wrapping_add(dg)
+                    .wrapping_sub(32),
+                curr.2
+                    .wrapping_add(dg)
+                    .wrapping_sub(40)
+                    .wrapping_add(dbdg),
+                curr.3,
+            ),
+            RUN(_) => curr, 
+        };
+        index[hash(curr) as usize] = curr;
+        let r = match chunk {
+            RUN(r) => r,
+            _ => 0,
+        };
+        for _ in 0..=r {
+            pixels.push(curr.3);
+            pixels.push(curr.2);
+            pixels.push(curr.1);
+            pixels.push(curr.0);
+        }
+    }
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let mut surface = Surface::new(
+    let mut surface = Surface::from_data(
+        &mut pixels,
         width,
         height,
+        width*4,
         RGBA8888,
     ).unwrap();
 
     let window = video_subsystem
         .window("Jasper's QOI Image Viewer", width, height)
         .position_centered()
-        .opengl()
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
     let texture_creator = canvas.texture_creator();
-
-    let mut i = 0;
-
-
-    surface.with_lock_mut(|v| {
-        for _j in 0..(width*height) as usize {
-            if let Some(p) = decode.next() {
-                v[i] = p.3;
-                v[i+1] = p.2;
-                v[i+2] = p.1;
-                v[i+3] = p.0;
-                i += 4;
-            } else {
-                break;
-            }
-        }
-    });
     let texture = surface.as_texture(&texture_creator).unwrap();
 
     canvas.copy(
